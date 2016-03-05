f5bb3351098fafda3ef99fcf5118b3e6d9d5d368
AstronautSloth <cjt1871@g.rit.edu>
2016-03-05 14:58:44 -0500

    Screen now displays what the camera sees, no control though

--- Red Eye/ViewController.swift
+++ Red Eye/ViewController.swift
@@ -7,12 +7,56 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let captureSession = AVCaptureSession()
+    var captureDevice : AVCaptureDevice?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        for device in devices{
+            if device.hasMediaType(AVMediaTypeVideo){
+                if device.position == AVCaptureDevicePosition.Back {
+                    captureDevice = device as? AVCaptureDevice
+                    if captureDevice != nil {
+                        beginSession()
+                    }
+                }
+            }
+        }
+        
+        
+        
+    }
+    
+    func beginSession() {
+        let err : NSError? = nil
+        do {
+            try captureSession.addInput(AVCaptureDeviceInput(device: captureDevice))
+        } catch _ {
+            NSLog("error \(err?.localizedDescription)")
+        }
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        self.view.layer.addSublayer(previewLayer)
+        previewLayer?.frame = self.view.layer.frame
+        captureSession.startRunning()
+    }
+    
+    func configureDevice() {
+        if let device = captureDevice {
+            do {
+                try device.lockForConfiguration()
+            }catch _ {
+                NSLog("Failed device lock")
+            }
+            device.focusMode = .Locked
+            device.unlockForConfiguration()
+        }
     }
 
     override func didReceiveMemoryWarning() {
